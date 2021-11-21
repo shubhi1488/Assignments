3fd8d5f6854fc8ed4de649eb765c794056be0943
HP <shubhi1488@gmail.com>
2021-11-21 20:57:44 +0530

    Question1

--- src/shape1.java
+++ src/shape1.java
@@ -1,2 +1,14 @@
 public class shape1 {
+    void draw(){
+        System.out.println("Drawing Shape");
+    }
+    void erase(){
+        System.out.println("Erasing shape");
+    }
+
+    public static void main(String[] args) {
+        shape1 s=new shape1();
+        s.draw();
+        s.erase();
+    }
 }
