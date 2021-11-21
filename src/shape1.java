a3ccb26179ce0a97b4ce387c5ff95122f7422771
HP <shubhi1488@gmail.com>
2021-11-21 20:59:35 +0530

    Question1

--- src/shape1.java
+++ src/shape1.java
@@ -1,3 +1,6 @@
+/* WAP to create a class named shape. It should contain two methods draw() and erase() that prints "Drawing
+Shape" and "Erasing Shape" respectively.
+ */
 public class shape1 {
     void draw(){
         System.out.println("Drawing Shape");
